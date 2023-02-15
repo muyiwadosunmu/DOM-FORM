85c01727783f6a52d5b02bb8290b297da5f75f84
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-02-15 10:42:31 +0100

    Initial push

---
+++ index.js
@@ -0,0 +1,11 @@
+const Submit = () => {
+    alert("This is Submit function");
+}
+
+const retrieveData = () => {
+    let namevariable = document.getElementById("name").value;
+    let emailVariable = document.getElementById("email").value;
+    let numberVariable = document.getElementById('phone').value;
+    let employeeIDVariable = document.getElementById("employeeID");
+    let joinedDate = document.getElementById('joinDate')
+}
